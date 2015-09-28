c37609fe0ca8e71a26700699efe6ce4cd1d926b7
Durgesh Kumar <kumardurgeshpatel@gmail.com>
2015-09-28 18:37:41 +0530

    user model

--- server/api/users.js
+++ server/api/users.js
@@ -1,18 +1,41 @@
 var express = require('express');
 var router = express.Router();
-
+var Users = require('./../models/Users');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-/* GET users is logined. */
+/**
+ * creating user
+ * @paran: None
+ * @reurn: Boolean
+ */
 router.get('/isLogined', function(req, res, next) {
   if(req.session && req.session.user) {
   	res.send(200,1);
   } else {
   	res.send(200,0);
   }
+});
+
+/**
+ * creating user
+ * @paran: Object
+ * @reurn: Object
+ */
+
+router.post('/create', function (req, res, next) {
+	Users.saveUser(req.body, function (err, user) {
+		if(err) {
+			console.log(err);
+			res.send(200, {success:false, err:err});
+		} else if(user) {
+			res.send(200, {success:true, user:user});
+		} else {
+			res.send(200, {success:false, err: "Please try again"});
+		}
+	});
 });
 
 module.exports = router;
--- server/models/users.js
+++ server/models/users.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var users = new Schema({
+var usersSchema = new Schema({
 	fname:  {
 		type:String,
 		require:true,
@@ -22,6 +22,39 @@ var users = new Schema({
 		trim:true
 	}
 });
-var Users = mongoose.model('Users', users);
+
+// saveing user
+usersSchema.statics.saveUser = function (userObj, cb) {
+	if(!userObj.email || userObj.email === undefined || typeof userObj.email === undefined)
+  		return cb("Invalid first name", null);
+  	if(!userObj.fname || userObj.fname === undefined || typeof userObj.fname === undefined)
+  		return cb("Invalid first name", null);
+  	if(!userObj.lname || userObj.lname === undefined || typeof userObj.lname === undefined)
+  		return cb("Invalid first name", null);
+  	if(!userObj.password || userObj.password === undefined || typeof userObj.password === undefined)
+  		return cb("Invalid first name", null);
+  	this.findOneByProp({email:userObj.email}, function (err, user) {
+  		// body...
+  		console.log(err, user);
+  		if(err)
+  			return cb(err, null);
+  		else if(user)
+  			return cb("email id already exist", null);
+  		else 
+  			return this.create(userObj, cb);
+  	});
+}
+
+usersSchema.statics.findOneByProp = function (query, cb) {
+	// body...
+
+	if(typeof query === 'object') {
+		return this.findOne(query, cb);
+	} else {
+		cb("Invalid search query", null);
+	}
+}
+
+var Users = mongoose.model('Users', usersSchema);
 
 module.exports = Users;
